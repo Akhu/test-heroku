3f03662962e14ab04b56d0d4f24b2fedead650e4
Anthony <me@anthony-dacruz.com>
2021-03-11 09:19:23 +0100

    Add Giphy Support with /gif {query} within chat

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const httpRequester = require('http');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-
+console.log(process.env.GIPHY_API_KEY);
 var users = [];
 
 var exampleUser = {
@@ -25,6 +26,10 @@ app.use(express.static('public'));
 
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
+    if (msg.includes('/gif')) {
+      let query = msg.split(' ')[1];
+      callGify(query);
+    }
     io.emit('chat message', msg);
   });
 });
@@ -32,3 +37,33 @@ io.on('connection', (socket) => {
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
+
+
+function callGify(query){
+  //https://api.giphy.com/v1/gifs/search?api_key=gUDP1beHuekoonSB2Nim24tYJ6BM0pas&q=cat&limit=1&offset=0&rating=g&lang=en
+  //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+  var options = {
+    host: 'api.giphy.com',
+    path: '/v1/gifs/search?api_key=' + process.env.GIPHY_API_KEY + '&q=' + query + '&limit=1&offset=0&rating=g&lang=en'
+  };
+
+  callback = function(response) {
+    var str = '';
+
+    //another chunk of data has been received, so append it to `str`
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+
+    //the whole response has been received, so we just print it out here
+    response.on('end', function () {
+      console.log(str);
+      
+      let jsonValue = JSON.parse(str);
+      console.log(jsonValue.data[0].images.downsized.url);
+      io.emit('gif', jsonValue.data[0].images.downsized.url);
+    });
+  }
+
+  httpRequester.request(options, callback).end();
+}
